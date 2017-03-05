75f0265bf8a81b44482981be01cfde13bbc258e4
YANSEN.XU <xys8804@gmail.com>
2017-03-05 21:22:45 +0900

    temp and sync with lab

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import cplex
 from nwgen import SubstrateNetwork, VirtualNetwork
 
@@ -19,36 +20,55 @@ class SubstrateNetworkData(object):
 substrateNetworkData = SubstrateNetworkData()
 substrateNetworkData.nbNodesOfSubstrateNetwork = 20
 substrateNetworkData.ctnProbability = 0.5
-substrateNetworkData.lbOfBandwidthCapacity = 100
+substrateNetworkData.lbOfBandwidthCapacity = 50
 substrateNetworkData.ubOfBandwidthCapacity = 100
-substrateNetworkData.lbOfCpuCapacity = 100
+substrateNetworkData.lbOfCpuCapacity = 50
 substrateNetworkData.ubOfCpuCapacity = 100
 
 sn = SubstrateNetwork( substrateNetworkData.nbNodesOfSubstrateNetwork, substrateNetworkData.ctnProbability)
 
 sn.setCpuCapacity( substrateNetworkData.lbOfCpuCapacity, substrateNetworkData.ubOfCpuCapacity )
 sn.setBandwidthCapacity( substrateNetworkData.lbOfBandwidthCapacity, substrateNetworkData.ubOfBandwidthCapacity )
-sn.setBandwidthCost(20)
+sn.setBandwidthCost(1, 20)
+sn.setCpuCost(1, 20)
 
 vn = VirtualNetwork(5, 0.5)
 vn.setBandwidthRequirement(1, 50)
 vn.setCpuRequirement(1, 50)
-print vn.number_of_nodes()
 
+
+#print( "number of nodes",vn.number_of_nodes() )
 
 
 def generateNodeDecisionVariable(vn, sn):
     dv = []
-    for i in range(0, vn.number_of_nodes()):
+    """
+     for i in range(0, vn.number_of_nodes()):
         for j in range(0, sn.number_of_nodes()):
             dv.append('Xv' + str(i) + 's' + str(j))
+    """
+    dv = [['Xv' + str(i) + 's' + str(j) for j in range(0, sn.number_of_nodes())] for i in range(0, vn.number_of_nodes())]
     return dv
+
+
+
 """
 def generateLinkDecisionVariable(vn, sn):
     dv = []
 """
 
-print generateNodeDecisionVariable(vn, sn)
+dv = generateNodeDecisionVariable(vn, sn)
+print(dv[0][2])
+
+obj = []
+for node in vn.nodes():
+    vn.node[node]['requirement']
+
+for i in vn.nodes():
+    for j in sn.nodes():
+        obj.append(dv[i][j])
+print('obj:', obj)
+
 
 prob = cplex.Cplex()
 prob.objective.set_sense(prob.objective.sense.minimize)
